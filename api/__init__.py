2a5b597a95a3230c4919a44d1664f00272515c9d
LSSPelegrino <lsspelegrino@gmail.com>
2019-09-27 01:09:59 -0300

    stray whitespace

--- api/__init__.py
+++ api/__init__.py
@@ -6,9 +6,8 @@ Initialize API Module
 from flask import Flask
 from flask_restful import Api
 
-
 app = Flask(__name__)
 api = Api(app)
 
 # # Recursive Imports
-from api import app, errors, calls
+from api import errors, calls
